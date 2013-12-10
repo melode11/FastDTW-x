8cc0a16050c288078f36fd01fc36ac4b79a40e01
melode11 <melode11@gmail.com>
2013-12-10 10:53:18 +0800

    refactor distancefunction

--- FastDTW-x/Classes/Util/DistanceFunctionFactory.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  DistanceFunctionFactory.cpp
-//  FastDTW-x
-//
-//  Created by Melo Yao on 12/2/13.
-//  Copyright (c) 2013 melo.yao. All rights reserved.
-//
-
-#include "DistanceFunctionFactory.h"
